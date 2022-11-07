0bb802279a42f55038481a54231af2857e640d33
climbingirl <isvetlanaivanova@yandex.ru>
2022-11-07 21:03:31 +0500

    feat: implement solution 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,24 @@
+const { readdir, mkdir, copyFile, rm } = require("fs/promises");
+const { join } = require("path");
 
+const targetDirName = join(__dirname, "files");
+const newDirName = join(__dirname, "files-copy");
+
+copyDir(targetDirName, newDirName);
+
+function copyDir(targetDir, newDir) {
+  mkdir(newDir, { recursive: true });
+
+  readdir(targetDir).then(files => {
+    files.forEach((file) => {
+        copyFile(join(targetDir, file), join(newDir, file));
+    });
+
+    readdir(newDir).then(copiedFiles => {
+      const extraFiles = copiedFiles.filter(i => !files.includes(i));
+      extraFiles.forEach(file => {
+        rm(join(newDir, file));
+      });
+    });
+  });
+}
